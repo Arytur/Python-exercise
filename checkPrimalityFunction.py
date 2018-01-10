3b93db6ea1b5031d848c476f89f5cd9348a3bbec
Arytur <artur.kozera@gmail.com>
2018-01-10 16:34:49 +0100

    check primality function

---
+++ checkPrimalityFunction.py
@@ -0,0 +1,16 @@
+def check_primality(range_numb, numb):
+    range_numb = [y for y in range_numb if numb % y == 0]
+    if range_numb:
+        return range_numb
+    else:
+        return "Empty. No divisors."
+
+
+number = int(input("Give me a number: "))
+# empty list for range
+d = []
+
+for x in range(2, number):
+    d.append(x)
+
+print("Divisors: ", check_primality(d, number))
