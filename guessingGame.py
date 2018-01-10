548f09bf1c7d296ff8c5d5374dc46c6dadf0579f
Arytur <artur.kozera@gmail.com>
2018-01-10 14:55:11 +0100

    add counter and exit game

--- guessingGame.py
+++ guessingGame.py
@@ -2,22 +2,29 @@ from random import randint
 
 ran_numb = randint(1, 9)
 
-guess = int(input("Guess a number from 1 to 9: "))
+guess = int(input("\nGuess a number from 1 to 9: "))
 
 
 def guess_numb(number1, number2):
     if number1 == number2:
         return "\n\n***Excellent! First chance and you won!***\n\n"
     else:
-        while number1 != number2:
-            if number1 > number2:
-                print("\nThe number is too low")
-                number2 = int(input("\nTry again... "))
+        count = 1
+        print("\nSorry, wrong number.\n\nDo you want to continue? y or n")
+        yesno = input('')
+        if yesno != 'n':
+            while number1 != number2:
+                count += 1
+                if number1 > number2:
+                    print("\nThe number %d is too low." % number2)
+                    number2 = int(input("\nTry again... "))
+                else:
+                    print("\nThe number %d is too high." % number2)
+                    number2 = int(input("\nTry again... "))
             else:
-                print("\nThe number is too high")
-                number2 = int(input("\nTry again... "))
-        else:
-            return "\n\n*That's right*\n\n"
+                return "\n\n*That's right. It's a number %d.*\n\n You needed %d tries to guess this number.\n" % (number1, count)
+        elif yesno == 'n':
+            return "Exit the game."
 
 
 print(guess_numb(ran_numb, guess))
