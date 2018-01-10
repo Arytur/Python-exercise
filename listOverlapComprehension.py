dcc049f820f6907b672726fba494b7ae2307733f
Arytur <artur.kozera@gmail.com>
2018-01-10 15:46:20 +0100

    shorter command

--- listOverlapComprehension.py
+++ listOverlapComprehension.py
@@ -5,14 +5,14 @@ b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 d = random.sample(range(40), 9)
 e = random.sample(range(55), 13)
 
-c = list(set([x for x in a for y in b if x == y]))
+c = list(set([x for x in a if x in b]))
 print("a =", a)
 print("b =", b)
 print("\ncommon value in [a] and [b] =", c, "\n")
 
 print("Random lists:\n")
 
-f = list(set([a for a in d for b in e if a == b]))
+f = list(set([a for a in d if a in e]))
 print("d =", sorted(d))
 print("e =", sorted(e))
 print("\ncommon value in [d] and [e] =", sorted(f), "\n")
