7401f8c4d30ba6f1df9987a2a048fca1633259e7
Arytur <artur.kozera@gmail.com>
2018-01-12 23:15:12 +0100

    fixed appearance of code

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -3,16 +3,12 @@ import string
 
 
 def generate_password():
-    print("How strong you want a password?")
-    print("1. Weak")
-    print("2. Medium")
-    print("3. Strong")
-    print("4. Very Strong")
+    print("How strong you want a password?\n1. Weak\n2. Medium\n3. Strong\n4. Very Strong")
     strength_choice = int(input('\n'))
     password = list()
     if strength_choice == 1:
-        strength = 5
-        while strength > 0:
+        length = 5
+        while length > 0:
             weak = random.choice('xxxxy')
             if weak == 'x':
                 weak_rand = random.choice(string.ascii_lowercase)
@@ -20,10 +16,10 @@ def generate_password():
             elif weak == 'y':
                 weak_rand = random.choice(string.digits)
                 password.append(weak_rand)
-            strength -= 1
+            length -= 1
     elif strength_choice == 2:
-        strength = 8
-        while strength > 0:
+        length = 8
+        while length > 0:
             medium = random.choice('xxxxyy')
             if medium == 'x':
                 medium_rand = random.choice(string.ascii_letters)
@@ -34,10 +30,10 @@ def generate_password():
             elif medium == 'z':
                 medium_rand = random.choice(string.punctuation)
                 password.append(medium_rand)
-            strength -= 1
+            length -= 1
     elif strength_choice == 3:
-        strength = 12
-        while strength > 0:
+        length = 12
+        while length > 0:
             strong = random.choice('xxxxxyyyyzz')
             if strong == 'x':
                 strong_rand = random.choice(string.ascii_letters)
@@ -48,10 +44,10 @@ def generate_password():
             elif strong == 'z':
                 strong_rand = random.choice(string.punctuation)
                 password.append(strong_rand)
-            strength -= 1
+            length -= 1
     elif strength_choice == 4:
-        strength = 18
-        while strength > 0:
+        length = 18
+        while length > 0:
             vstrong = random.choice('xxxxyyyyzzz')
             if vstrong == 'x':
                 vstrong_rand = random.choice(string.ascii_letters)
@@ -62,7 +58,7 @@ def generate_password():
             elif vstrong == 'z':
                 vstrong_rand = random.choice(string.punctuation)
                 password.append(vstrong_rand)
-            strength -= 1
+            length -= 1
     return ''.join(password)
 
 
